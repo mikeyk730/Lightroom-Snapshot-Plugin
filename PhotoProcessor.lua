758d97e1516858a4064b43da4a552199d8d4b3a6
unknown <mikeyk730@gmail.com>
2016-05-01 18:03:56 -0400

    simplify plugin

--- PhotoProcessor.lua
+++ PhotoProcessor.lua
@@ -38,18 +38,8 @@ function PhotoProcessor.promptForSnapshotName()
 end
 
 
---Create a develop snapshot fro the supplied photo.  If no name is supplied
---the user will be prompted with a dialog
---todo:move to standalone plugin
-function PhotoProcessor.runCommandCreateSnapshot(photo, name)
-   logger:trace("Entering runCommandCreateSnapshot", photo.path)
-   
-   --todo:
-   if name == nil then
-      logger:trace("Snapshot canceled", photo.path)
-      return
-   end
-
+--Create a develop snapshot for the supplied photo.
+function PhotoProcessor.createSnapshot(photo, name)
    logger:trace("Creating snapshot", name, photo.path)
    local catalog = LrApplication.activeCatalog()
    catalog:withWriteAccessDo("Create Snapshot", function(context) 
@@ -58,39 +48,15 @@ function PhotoProcessor.runCommandCreateSnapshot(photo, name)
 end
 
 
-function PhotoProcessor.promptUser(action)
-   local props = {}
-
-   if action == "createSnapshot" then
-      props.name = PhotoProcessor.promptForSnapshotName()
-   end
-
-   return props
-end
-
-
-function PhotoProcessor.processPhoto(photo, action, props)
-   --LrTasks.startAsyncTask(function(context)
+function PhotoProcessor.startSnapshotTask(photo, name)
+   LrTasks.startAsyncTask(function(context)
          local available = photo:checkPhotoAvailability()
          if available then         
-
-            --Skip files that are not Canon Raw files
-            local ft = photo:getFormattedMetadata('fileType')
-            local make = photo:getFormattedMetadata('cameraMake')
-            if ft ~= 'Raw' or make ~= 'Canon' then
-               logger:trace("Skipping unsupported file", make, ft, photo.path)
-               return
-            end
-
-         if action == "createSnapshot" then
-               PhotoProcessor.runCommandCreateSnapshot(photo, props.name)
-            else
-               logger:error("Unknown action: " .. action)
-            end
+            PhotoProcessor.createSnapshot(photo, name)
          else
             logger:warn("Photo not available: " .. photo.path)
          end
-   --end)
+   end)
 end
 
 
@@ -109,30 +75,33 @@ end
 
 
 --Main entry point for scripts associated with menu items
-function PhotoProcessor.processPhotos(action)
+function PhotoProcessor.processPhotos()
    local photos = PhotoProcessor.getSelectedPhotos()
    local totalPhotos = #photos
-   logger:trace("Starting task", action, totalPhotos)
-   --todo:rework so each photo can be a task
-   LrTasks.startAsyncTask(function(context)
-         local progressScope = LrProgressScope {title=action.." "..totalPhotos.." photos"}
-         --todo:doens't work, progress hangs on error
-         --progressScope:attachToFunctionContext(context)
-         progressScope:setCancelable(true)
+   logger:trace("processPhotos", totalPhotos)
 
-         local props = PhotoProcessor.promptUser(action)
+   local name = PhotoProcessor.promptForSnapshotName()
+   if name == nil then:
+      logger:trace("No name provided")
+      return
+   end
 
-         for i,v in ipairs(photos) do
-            if progressScope:isCanceled() then 
-               logger:trace("Canceled task", action, i)
-               break
-            end
-            progressScope:setPortionComplete(i, totalPhotos)
-            progressScope:setCaption(action.." "..i.." of "..totalPhotos)
-            PhotoProcessor.processPhoto(v, action, props)
-         end
+   local progressScope = LrProgressScope {title="Create Snapshot For  "..totalPhotos.." Photos"}
+   --todo:doens't work, progress hangs on error
+   --progressScope:attachToFunctionContext(context)
+   progressScope:setCancelable(true)
 
-         logger:trace("Completed task", action, totalPhotos)
-         progressScope:done()
-   end)
+   for i,photo in ipairs(photos) do
+      if progressScope:isCanceled() then 
+         logger:trace("Canceled task", i)
+         break
+      end
+      progressScope:setPortionComplete(i, totalPhotos)
+      progressScope:setCaption("Snapshot "..i.." of "..totalPhotos)
+
+      PhotoProcessor.startSnapshotTask(photo, name)
+   end
+
+   logger:trace("Completed task", totalPhotos)
+   progressScope:done()
 end
